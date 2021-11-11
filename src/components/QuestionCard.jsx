ae2035bf1f1a9952b22b3e018a166db12efa09fc
Fabian2411 <fabian.backhaus1@gmx.de>
2021-11-11 10:42:08 +0100

    Fabian: Question2

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../assets/css/style.css";
 import styles from "../assets/css/QuestionCard.module.css";
 
 const QuestionCard = () => {
     return (
-        <div>
-            <h3>Poll question</h3>
-            <h4>Poll choice</h4>
-            <h4>Poll choice</h4>
-            <h4>Poll choice</h4>
+        <div className={styles.allQuestionCard}>
+            <h3 className={styles.question}>Do you believe the climate is changing?</h3>
+            <div className={styles.answers}>
+                <h4 className={styles.answerOne}>Yes</h4>
+                <h4 className={styles.answerTwo}>The climate is changing?</h4>
+                <h4 className={styles.answerThree}>No</h4>
+            </div>
+            {/* Skipp zur nächsten Frage */}
+            <Link to="/question">
+                SKIP {">>"}
+            </Link>
         </div>
     );
 };
