e7563bf42f7a453d185ec7938bf5600f8eccd3fd
uxvu1196 <uxvu1196@gmail.com>
2021-11-12 11:23:27 +0100

    uyen contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,28 +1,49 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import "../assets/css/Contact.css"
 
 export default function Contact() {
     return (
-        <section>
-            <img src="/img/logo_1.png" alt="We Super People Logo" />
-            {/* ============================== Start of left Area ================================= */}
-            <div className="leftArea">
-                <h1>We Super People</h1>
+        <div>
+            <section className="contact">
+                <div>
+                    {/* ============================== Start of left Area ================================= */}
+                    <div className="leftArea">
+                        <img src="/img/logo_1.png" alt="We Super People Logo" />
 
-                <p>
-                    <span>Mitmachen</span> <br />
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum similique corrupti itaque architecto minus laboriosam obcaecati repellat totam quidem nisi dicta sit beatae numquam, voluptas odit odio qui ipsum repudiandae.
-                </p>
+                        <div className="leftAreaText">
+                            <h1>We Super <br /> People</h1>
+
+                            <p>
+                                <span>Mitmachen</span> <br />
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.Harum similique corrupti itaque architecto minus laboriosam obcaecati repellat totam quidem nisi dicta sit beatae numquam, voluptas odit odio qui ipsum repudiandae.
+                            </p>
+                        </div>
+                    </div>
+
+                    {/* ============================== Start of right Area ================================= */}
+                    <div className="rightArea">
+                        <form action="">
+                            <fieldset className="formBox">
+                                <label htmlFor="Name"></label>
+                                Name <span>*</span> <br /> <input name="name" id="vorname" placeholder="Enter text" />
+                                <br />
+                                Email <span>*</span> <br /> <input type="email" name="email" id="email" placeholder="Enter text" />
+                                <br />
+                                Nachricht <span>*</span> <br /> <input type="text" id="message" />
+                                <br />
+                                <Link to="/contact_thanks" className="#">Senden</Link>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </section>
+
+            <div className="sidebar">
+                <h1>SIDEBAR</h1>
             </div>
 
-            {/* ============================== Start of right Area ================================= */}
-            {/* <div className="rightArea">
-                <form action="">
-
-
-                </form>
-            </div> */}
-        </section>
+        </div>
     )
 }
 
