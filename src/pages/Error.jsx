0ceaf0070f0a566f6cc20602906fa7c2081c11fc
Fabian2411 <fabian.backhaus1@gmx.de>
2021-11-15 11:38:56 +0100

    Fabi: Login

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
     return (
         <div className={styles.allError}>
             <div className={styles.text}>
-                <h1 className={styles.error}>💩 Error<span className={styles.webStrokeError}> &nbsp;404</span>&nbsp;💩</h1>
+                <h1 className={styles.error}>🚫 Error<span className={styles.webStrokeError}> &nbsp;404</span>&nbsp;🚫</h1>
                 <p className={styles.errorText}>Deine Gedanken konnten nicht mit der Community Super People geteilt werden! you will be redirectd to the question in 3s</p>
             </div>
         </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from "../assets/css/Login.module.css";
+import SuperPeople from '../components/SuperPeople';
+import LoginCard from '../components/LoginCard';
 
 export default function Login() {
     return (
-        <div>
-            <h1 className={styles.test}>Login</h1>
+        <div className={styles.allLogin}>
+            {/* <SuperPeople /> */}
+            <LoginCard className={styles.componentLoginCard} />
         </div>
     )
 }
