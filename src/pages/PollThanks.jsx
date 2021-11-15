4676e38112b4c1359cb59b9710630ee558c6d1c8
DevilRaider-Dev <deviltwix3@gmail.com>
2021-11-15 14:49:43 +0100

    Andi: Restore PollThankls

--- src/pages/PollThanks.jsx
+++ src/pages/PollThanks.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from "../assets/css/PollThanks.module.css";
 
 export default function PollThanks() {
     return (
-        <div>
-            <h1>PollThanks</h1>
+        <div className={styles.allPollThanks}>
+            <div className={styles.text}>
+                <h1 className={styles.yeah}>🎉 YEAH<span className={styles.webStrokeYeah}> &nbsp;YEAH</span>&nbsp;🎉</h1>
+                <p className={styles.deineGedanken}>Deine Gedanken sind mit der Community Super People geteilt! you will be redirectd to the result in 3s</p>
+            </div>
         </div>
     )
 }
+
+// http://localhost:3000/poll_thanks
