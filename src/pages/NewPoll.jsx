9a9ef05a4ba01bbb86af81843b517298e8ed7df2
enriquenv <enriquen@live.de>
2021-11-14 23:59:40 -1000

    NewPoll Seite

--- src/pages/NewPoll.jsx
+++ src/pages/NewPoll.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import ContactSidebar from "../components/ContactSidebar";
+import NextRandom from "../components/NextRandom";
+import ResultContainer from "../components/ResultContainer";
+import "../assets/css/style.css";
+import styles from "../assets/css/NewPoll.module.css";
 
-export default function NewPoll() {
+const NewPoll = () => {
     return (
-        <div>
-            <h1>NewPoll</h1>
+        <div className={styles.parent}>
+            {/* Home - Todo: Sidebar will be moved to app.js */}
+            <ContactSidebar />
+            {/* ============================== Start of left Area ================================= */}
+            <section className={styles.mainsectionleft}>
+                <img
+                    src="./img/logo_1.png"
+                    alt="We Super People logo"
+                    className={styles.logo}
+                />
+                <h1 className={styles.h1}>
+                    We Super
+                    <br /> &nbsp;&nbsp;&nbsp;People
+                </h1>
+            </section>
+            {/* ============================== Start of right Area ================================= */}
+            <section className={styles.mainsectionright}>
+                {/* here component for charts, survey, etc */}
+                <ResultContainer />
+            </section>
         </div>
-    )
-}
+    );
+};
+
+export default NewPoll;
