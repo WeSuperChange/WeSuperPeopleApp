524164e2bbd0ef61109fb88a90c415335906c07e
uxvu1196 <uxvu1196@gmail.com>
2021-11-16 10:34:51 +0100

    uyen

--- src/pages/ContactThanks.jsx
+++ src/pages/ContactThanks.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import styles from "../assets/css/ContactThanks.module.css"
 
 export default function ContactThanks() {
     return (
-        <div>
-            <h1>DANKE</h1>
+        <div className={styles.contactThanks}>
+            <div className={styles.text}>
+                <h1 className={styles.thx}>YEAH <span className={styles.webStrokeContactThx}> THANKS</span></h1>
+                <p className={styles.contactThanksText}>Deine Anfrage ist mit der Community We Super People geteilt! Wir werden die Nachricht so schnell wie möglich bearbeiten und Dir zurück schreiben ;-)</p>
+            </div>
         </div>
     )
 }
