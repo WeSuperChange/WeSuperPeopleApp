771aded11a1987a2261f77e00837a028b71678da
uxvu1196 <uxvu1196@gmail.com>
2021-11-17 11:06:19 +0100

    uyen contact thanks

--- src/pages/ContactThanks.jsx
+++ src/pages/ContactThanks.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
-import { useState } from "react";
-import { Navigate } from "react-router-dom";
+// import { useState } from "react";
+// import { Navigate } from "react-router-dom";
 import styles from "../assets/css/ContactThanks.module.css"
+import { Link } from "react-router-dom";
+
 
 export default function ContactThanks() {
-    const [redirectNow, setRedirectNow] = useState(false);
-    setTimeout(() => setRedirectNow(true), 3000);
-    return redirectNow ? (
-        <Navigate to="/" />
-    ) : (
+    // const [redirectNow, setRedirectNow] = useState(false);
+    // setTimeout(() => setRedirectNow(true), 3000);
+    // return redirectNow ? (
+    //     <Navigate to="/" />
+    // ) : (
+    return (
         <div className={styles.contactThanks}>
             <div className={styles.text}>
-                <h1 className={styles.thx}>YEAH <span className={styles.webStrokeContactThx}> THANKS</span></h1>
+                <h1 className={styles.thx}>🎉YEAH <span className={styles.webStrokeContactThx}> THANKS</span>🎉</h1>
                 <p className={styles.contactThanksText}>Deine Anfrage ist mit der Community We Super People geteilt! Wir werden die Nachricht so schnell wie möglich bearbeiten und Dir zurück schreiben ;-)</p>
             </div>
+            <div className="Sidebar">
+                <Link to="/" className="link" >
+                    {"<<"}
+                </Link>
+            </div>
         </div>
+
     )
 }
