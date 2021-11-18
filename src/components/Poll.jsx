09ea375b0be2f917d3d72369cdc267b3ffa15ac3
DevilRaider-Dev <deviltwix3@gmail.com>
2021-11-18 14:37:17 +0100

    andi: letsgo question

--- src/components/Poll.jsx
+++ src/components/Poll.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../assets/css/Poll.module.css";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Poll(props) {
 
