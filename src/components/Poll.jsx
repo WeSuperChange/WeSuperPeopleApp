44ef9f6fec946b834a37cc17a9c6bb2fc15bec4a
Fabian2411 <fabian.backhaus1@gmx.de>
2021-11-18 12:14:35 +0100

    Enrique, Fabi: Verbesserungen

--- src/components/Poll.jsx
+++ src/components/Poll.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../assets/css/Poll.module.css";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Poll(props) {
     const [questionGo, setQuestionGo] = useState("");
@@ -14,6 +15,10 @@ export default function Poll(props) {
             <div hidden={true}>{props.id}</div>
             <div className={styles.category}>{props.category}</div>
             <h3 className={styles.question}>{props.question}</h3>
+            {/* <Link to="/results" className={styles.arrows}> */}
+            <img src="./img/arrows.png" alt="" className={styles.arrows} />
+            {/* </Link> */}
         </div>
+
     );
 }
